d8d7d7fe8f413f88b3aa3f822b5b7e6b4a84f1a9
Yann Bemba <yann@soloso.fr>
2021-12-02 11:48:04 +0100

    setting sql database

---
+++ app/src/main/java/com/example/notecompose/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,15 @@
+package com.example.notecompose.feature_note.data.data_source
+
+import androidx.room.Database
+import com.example.notecompose.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class,],
+    version = 1
+)
+abstract class NoteDatabase {
+
+    abstract val noteDao: NoteDao
+
+}
+
